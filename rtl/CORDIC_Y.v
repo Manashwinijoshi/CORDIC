876bfe25f9cd7a68413b51e724d58918b6d407b8
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-01-30 04:47:51 +0530

    Update CORDIC_Y.v

--- rtl/CORDIC_Y.v
+++ rtl/CORDIC_Y.v
@@ -1,31 +1,23 @@
 module CORDIC_Y (
-    input  wire signed [15:0] Y_i,
-    input  wire signed [15:0] Shifted_x,
-    input  wire               Enable,
-    output reg  signed [15:0] Cordic_y
+    input wire Clk,
+    input wire Reset,
+    input wire signed [15:0] Y_i,
+    input wire signed [15:0] Shifted_x,
+    input wire Enable,
+    output wire signed [15:0] Cordic_y
 );
 
-    reg signed [16:0] temp_y;
-
-    always @(*) begin
-        
-        if (Enable == 1'b0) begin
-            temp_y = Y_i + Shifted_x;
-        end
-        else begin
-            temp_y = Y_i - Shifted_x;
-        end
-        
-        if (temp_y > 17'sd32767) begin
-            Cordic_y = 16'sd32767;
-        end
-        else if (temp_y < -17'sd32768) begin
-            Cordic_y = -16'sd32768;
-        end
-        else begin
-            Cordic_y = temp_y[15:0];
-        end
-        
-    end
+    wire enable_inverted;
+    
+    assign enable_inverted = ~Enable;
+    
+    ADD_SUB adder_y (
+        .Clk(Clk),
+        .Reset(Reset),
+        .Input_a(Y_i),
+        .Input_b(Shifted_x),
+        .Enable(enable_inverted),
+        .Output_res(Cordic_y)
+    );
 
 endmodule

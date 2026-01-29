f3b19765f918701ca7dfc336fddc62b7f5957770
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-01-30 04:49:27 +0530

    Update PRE_PROCESSING_UNIT.v

--- rtl/PRE_PROCESSING_UNIT.v
+++ rtl/PRE_PROCESSING_UNIT.v
@@ -1,43 +1,51 @@
 module PRE_PROCESSING_UNIT (
-    input  wire [16:0] Input_angle,
-    output reg  [15:0] Reduced_angle,
-    output reg         Cos_negate,
-    output reg         Sin_negate
+    input wire signed [17:0] Input_angle,
+    output reg signed [15:0] Reduced_angle,
+    output reg Cos_negate
 );
 
-    localparam [16:0] ANGLE_PI_2    = 17'h06488;
-    localparam [16:0] ANGLE_PI      = 17'h0C910;
-    localparam [16:0] ANGLE_PI_3_2  = 17'h12D98;
-    localparam [16:0] ANGLE_TWO_PI  = 17'h19220;
+    localparam signed [17:0] ANGLE_ZERO = 18'sh00000; // 0
+    localparam signed [17:0] ANGLE_PI_2 = 18'sh06488; // 90° = 25736
+    localparam signed [17:0] ANGLE_PI = 18'sh0C910; // 180° = 51472
+    localparam signed [17:0] ANGLE_PI_3_2 = 18'sh12D98; // 270° = 77208
+    localparam signed [17:0] ANGLE_TWO_PI = 18'sh19220; // 360° = 102944
+
+    reg signed [17:0] normalized_angle;
 
     always @(*) begin
         
-        if (Input_angle >= ANGLE_TWO_PI) begin
-            Reduced_angle = Input_angle - ANGLE_TWO_PI;
+        normalized_angle = Input_angle;
+        
+        // angles >= 360
+        if (normalized_angle >= ANGLE_TWO_PI) begin
+            normalized_angle = normalized_angle - ANGLE_TWO_PI;
         end
-        else begin
-            Reduced_angle = Input_angle[15:0];
+        
+        // angles < 0
+        if (normalized_angle < ANGLE_ZERO) begin
+            normalized_angle = normalized_angle + ANGLE_TWO_PI;
         end
 
-        if (Input_angle < ANGLE_PI_2) begin
-            Reduced_angle = Input_angle[15:0];
+        // Quadrant I: (0° to 90°)
+        if (normalized_angle <= ANGLE_PI_2) begin
+            Reduced_angle = normalized_angle[15:0];
             Cos_negate = 1'b0;
-            Sin_negate = 1'b0;
         end
-        else if (Input_angle < ANGLE_PI) begin
-            Reduced_angle = ANGLE_PI - Input_angle;
+        // Quadrant II: (90° to 180°)
+        else if (normalized_angle <= ANGLE_PI) begin
+            Reduced_angle = ANGLE_PI - normalized_angle;
             Cos_negate = 1'b1;
-            Sin_negate = 1'b0;
         end
-        else if (Input_angle < ANGLE_PI_3_2) begin
-            Reduced_angle = Input_angle - ANGLE_PI;
+        // Quadrant III: (180° to 270°)
+        else if (normalized_angle <= ANGLE_PI_3_2) begin
+            Reduced_angle = normalized_angle - ANGLE_PI;
+            Reduced_angle = -Reduced_angle;
             Cos_negate = 1'b1;
-            Sin_negate = 1'b1;
         end
+        // Quadrant IV: (270° to 360°)
         else begin
-            Reduced_angle = ANGLE_TWO_PI - Input_angle;
+            Reduced_angle = normalized_angle - ANGLE_TWO_PI;
             Cos_negate = 1'b0;
-            Sin_negate = 1'b1;
         end
 
     end

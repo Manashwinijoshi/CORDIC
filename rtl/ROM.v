e1c32cda02b443922557cfa28920c0804efaea4a
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-01-30 04:57:57 +0530

    Update ROM.v

--- rtl/ROM.v
+++ rtl/ROM.v
@@ -1,27 +1,27 @@
 module ROM (
-    input  wire [3:0]  Address,
-    output reg  [15:0] Data
+    input wire [3:0] addr,
+    output reg [31:0] data
 );
 
     always @(*) begin
-        case (Address)
-            4'd0  : Data = 16'd12867;
-            4'd1  : Data = 16'd7596;
-            4'd2  : Data = 16'd4013;
-            4'd3  : Data = 16'd2037;
-            4'd4  : Data = 16'd1022;
-            4'd5  : Data = 16'd511;
-            4'd6  : Data = 16'd256;
-            4'd7  : Data = 16'd128;
-            4'd8  : Data = 16'd64;
-            4'd9  : Data = 16'd32;
-            4'd10 : Data = 16'd16;
-            4'd11 : Data = 16'd8;
-            4'd12 : Data = 16'd4;
-            4'd13 : Data = 16'd2;
-            4'd14 : Data = 16'd1;
-            4'd15 : Data = 16'd1;
-            default : Data = 16'd0;
+        case (addr)
+            4'd0  : data = 32'h3F490FDB;
+            4'd1  : data = 32'h3EED6338;
+            4'd2  : data = 32'h3E7ADBB0;
+            4'd3  : data = 32'h3DFEADD5;
+            4'd4  : data = 32'h3D7FAADE;
+            4'd5  : data = 32'h3CFFEAAE;
+            4'd6  : data = 32'h3C7FFAAB;
+            4'd7  : data = 32'h3BFFFEAB;
+            4'd8  : data = 32'h3B7FFFAB;
+            4'd9  : data = 32'h3AFFFFEB;
+            4'd10 : data = 32'h3A7FFFFB;
+            4'd11 : data = 32'h39FFFFFF;
+            4'd12 : data = 32'h397FFFFF;
+            4'd13 : data = 32'h38FFFFFF;
+            4'd14 : data = 32'h387FFFFF;
+            4'd15 : data = 32'h37FFFFFF;
+            default : data = 32'h00000000;
         endcase
     end
 

f91cefde8f76f412a97158f72536535766b76f11
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-01-30 04:48:17 +0530

    Update CORDIC_Z.v

--- rtl/CORDIC_Z.v
+++ rtl/CORDIC_Z.v
@@ -1,19 +1,20 @@
+
 module CORDIC_Z (
-    input  wire signed [15:0] Z_i,
-    input  wire signed [15:0] Arctan,
-    input  wire               Enable,
-    output reg  signed [15:0] Cordic_z
+    input wire Clk,
+    input wire Reset,
+    input wire signed [15:0] Z_i,
+    input wire signed [15:0] Arctan,
+    input wire Enable,
+    output wire signed [15:0] Cordic_z
 );
 
-    always @(*) begin
-        
-        if (Enable == 1'b0) begin
-            Cordic_z = Z_i - Arctan;
-        end
-        else begin
-            Cordic_z = Z_i + Arctan;
-        end
-        
-    end
+    ADD_SUB adder_z (
+        .Clk(Clk),
+        .Reset(Reset),
+        .Input_a(Z_i),
+        .Input_b(Arctan),
+        .Enable(Enable),
+        .Output_res(Cordic_z)
+    );
 
 endmodule

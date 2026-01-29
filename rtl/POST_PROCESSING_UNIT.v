2f7cda078aa96074c1a3731e58fb715c46339ae2
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-01-30 04:54:22 +0530

    Update POST_PROCESSING_UNIT.v

--- rtl/POST_PROCESSING_UNIT.v
+++ rtl/POST_PROCESSING_UNIT.v
@@ -1,90 +1,37 @@
-
-module PRE_PROCESSING_UNIT (
+odule POST_PROCESSING_UNIT (
     input wire clk,
     input wire rst,
-    input wire [31:0] input_angle,
-    output reg [31:0] registered_angle,
-    output reg cos_neg,
-    output reg sin_neg
+    input wire [31:0] cordic_x,
+    input wire [31:0] cordic_y,
+    input wire cos_neg,
+    input wire sin_neg,
+    output reg [31:0] cos,
+    output reg [31:0] sin
 );
 
-    localparam [31:0] FP_PI_2 = 32'h3FC90FDB;
-    localparam [31:0] FP_PI = 32'h40490FDB;
-    localparam [31:0] FP_3PI_2 = 32'h4096CBE4;
-    localparam [31:0] FP_2PI = 32'h40C90FDB;
-
-    wire [31:0] angle_abs;
-    wire input_neg;
-    
-    wire gt_pi_2, gt_pi, gt_3pi_2, gt_2pi;
-    
-    wire [31:0] pi_sub_angle;
-    wire [31:0] angle_sub_pi;
-    wire [31:0] twopi_sub_angle;
-    
-    reg [31:0] reduced_angle;
-    reg cos_neg_calc;
-    reg sin_neg_calc;
-
-    assign input_neg = input_angle[31];
-    assign angle_abs = {1'b0, input_angle[30:0]};
-
-    FP_COMPARE_GT cmp_pi_2 (.a(angle_abs), .b(FP_PI_2), .gt(gt_pi_2));
-    FP_COMPARE_GT cmp_pi (.a(angle_abs), .b(FP_PI), .gt(gt_pi));
-    FP_COMPARE_GT cmp_3pi_2 (.a(angle_abs), .b(FP_3PI_2), .gt(gt_3pi_2));
-    FP_COMPARE_GT cmp_2pi (.a(angle_abs), .b(FP_2PI), .gt(gt_2pi));
-
-    FP_SUB_COMB sub1 (.a(FP_PI), .b(angle_abs), .result(pi_sub_angle));
-    FP_SUB_COMB sub2 (.a(angle_abs), .b(FP_PI), .result(angle_sub_pi));
-    FP_SUB_COMB sub3 (.a(FP_2PI), .b(angle_abs), .result(twopi_sub_angle));
+    reg [31:0] cos_work;
+    reg [31:0] sin_work;
 
     always @(*) begin
-        reduced_angle = angle_abs;
-        cos_neg_calc = 1'b0;
-        sin_neg_calc = 1'b0;
-        
-        if (gt_2pi) begin
-            reduced_angle = 32'h00000000;
-            cos_neg_calc = 1'b0;
-            sin_neg_calc = 1'b0;
-        end
-        else if (gt_3pi_2) begin
-            reduced_angle = twopi_sub_angle;
-            reduced_angle[31] = 1'b1;
-            cos_neg_calc = 1'b0;
-            sin_neg_calc = 1'b0;
-        end
-        else if (gt_pi) begin
-            reduced_angle = angle_sub_pi;
-            cos_neg_calc = 1'b1;
-            sin_neg_calc = 1'b1;
-        end
-        else if (gt_pi_2) begin
-            reduced_angle = pi_sub_angle;
-            cos_neg_calc = 1'b1;
-            sin_neg_calc = 1'b0;
-        end
-        else begin
-            reduced_angle = angle_abs;
-            cos_neg_calc = 1'b0;
-            sin_neg_calc = 1'b0;
-        end
-
-        if (input_neg) begin
-            sin_neg_calc = ~sin_neg_calc;
-        end
+        if (cos_neg)
+            cos_work = {~cordic_x[31], cordic_x[30:0]};
+        else
+            cos_work = cordic_x;
+            
+        if (sin_neg)
+            sin_work = {~cordic_y[31], cordic_y[30:0]};
+        else
+            sin_work = cordic_y;
     end
 
     always @(posedge clk) begin
         if (rst) begin
-            registered_angle <= 32'h00000000;
-            cos_neg <= 1'b0;
-            sin_neg <= 1'b0;
+            cos <= 32'h00000000;
+            sin <= 32'h00000000;
         end
         else begin
-            registered_angle <= reduced_angle;
-            cos_neg <= cos_neg_calc;
-            sin_neg <= sin_neg_calc;
+            cos <= cos_work;
+            sin <= sin_work;
         end
     end
 
